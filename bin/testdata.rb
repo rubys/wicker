bb454ddfa7992f9fcebffe92bd6e98305fd17419
Sam Ruby <rubys@intertwingly.net>
2014-12-31 21:06:15 -0500

    add comment test data

--- bin/testdata.rb
+++ bin/testdata.rb
@@ -10,6 +10,9 @@ FileUtils.mkdir_p dest
 posts = %w(one two three four five six seven eight nine ten eleven twelve)
 
 posts.each_with_index do |name, number|
+  post = "#{dest}/#{name}.txt"
+  next if File.exist? post
+
   codepoint = 0x2460 + number
   number += 1
   time = Time.parse("2014-#{number}-#{number} #{number}:#{number}:#{number}")
@@ -23,7 +26,6 @@ posts.each_with_index do |name, number|
   svg.at('g')['transform'] = 'scale(0.408) translate(-21,-175)'
 
   puts name
-  post = "#{dest}/#{name}.txt"
   File.open(post, 'w') do |file|
     file.puts name.capitalize
     file.puts "<div class='excerpt'>#{([name]*[number,3].min).join(' ')}</div>"
@@ -31,4 +33,26 @@ posts.each_with_index do |name, number|
     file.puts "<p>#{([name]*number).join(' ')}</p>"
   end
   File.utime(time, time, post)
+end
+
+time = Time.parse('2014-12-12 12:12:12').utc
+posts.each_with_index do |name, number|
+  time += 60*60
+  comment = "#{dest}/twelve-#{time.to_i}.cmt"
+  next if File.exist? comment
+  File.open(comment, 'w') do |file|
+    file.puts 'Twelve'
+    file.puts name
+  end
+  File.utime(time, time, comment)
+end
+
+time = Time.parse('2014-05-05 05:05:05').utc + 60*60
+comment = "#{dest}/five-#{time.to_i}.cmt"
+unless File.exist? comment
+  File.open(comment, 'w') do |file|
+    file.puts 'Five'
+    file.puts 'comment'
+  end
+  File.utime(time, time, comment)
 end
--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -10,12 +10,14 @@ feature "index" do
     visit '/'
     expect(page).to have_selector 'p', text: (%w(twelve)*12).join(' ')
     expect(page).to have_xpath '//a[@href="2014/12/12/twelve"]'
-    expect(page).to have_selector 'a', text: 'Add comment'
+    expect(page).to have_selector 'a', text: '12 comments'
   end
 
   it "should only have excerpts for the other items" do
     visit '/'
     expect(page).to have_selector 'p', text: /\Aeleven eleven eleven\Z/
     expect(page).to have_selector 'p', text: /\Athree three three\Z/
+    expect(page).to have_selector 'a', text: 'Add comment'
+    expect(page).to have_selector 'a', text: /\A1 comment\Z/
   end
 end
